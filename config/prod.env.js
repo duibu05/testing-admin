17c18e9e1e81da01dcd3332e5603b3d95bf12089
卢纪霖 <duibu05@126.com>
2017-12-24 23:54:14 +0800

    update prod config

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,10 +1,10 @@
 module.exports = {
     NODE_ENV: '"production"',
     ENV_CONFIG: '"prod"',
-    BASE_API: '"https://www.easy-mock.com/mock/5a06bcde7db3d453b0bdb989/testing/api"',
-    APP_ORIGIN: '"https://pc.nightka.com"',
-    QINIU_DOWNLOAD_DOMAIN: '"http://cdn1.ccf-china.com/"',
+    BASE_API: '"http://api.gdpassing.com/v1"',
+    APP_ORIGIN: '"http://admin.gdpassing.com"',
+    QINIU_DOWNLOAD_DOMAIN: '"http://cdn.gdpassing.com/"',
     QINIU_UPLOAD_DOMAIN: '"http://up-na0.qiniu.com/"', 
-    QINIU_TOKEN_URL: '"http://en.admin.ccf-china.com/api/qiniu-uptoken"'
+    QINIU_TOKEN_URL: '"http://api.gdpassing.com/v1/qiniu-uptoken"'
 };
 // TODO 生产环境请替换正式路径
