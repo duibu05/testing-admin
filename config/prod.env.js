3b5c4c0134b44486b3d68a1fd43799f2695a2026
duibu05 <duibu05@126.com>
2017-12-27 11:44:13 +0800

    Update prod.env.js
    update qiniu up domain

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,7 +4,7 @@ module.exports = {
     BASE_API: '"http://api.gdpassing.com/v1"',
     APP_ORIGIN: '"http://admin.gdpassing.com"',
     QINIU_DOWNLOAD_DOMAIN: '"http://cdn.gdpassing.com/"',
-    QINIU_UPLOAD_DOMAIN: '"http://up-na0.qiniu.com/"', 
+    QINIU_UPLOAD_DOMAIN: '"http://up-z2.qiniu.com/"', 
     QINIU_TOKEN_URL: '"http://api.gdpassing.com/v1/qiniu-uptoken"'
 };
 // TODO 生产环境请替换正式路径
