131a964098ef7386608a3f190efcaff0299d91a2
duibu05 <duibu05@126.com>
2017-12-27 11:45:02 +0800

    Update dev.env.js

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,7 +3,7 @@ module.exports = {
     ENV_CONFIG: '"dev"',
     BASE_API: '"http://localhost:3000/v1"',
     APP_ORIGIN: '"https://pc.nightka.com"',
-    QINIU_DOWNLOAD_DOMAIN: '"http://cdn1.ccf-china.com/"',
-    QINIU_UPLOAD_DOMAIN: '"http://up-na0.qiniu.com/"',
+    QINIU_DOWNLOAD_DOMAIN: '"http://cdn.gdpassing.com/"',
+    QINIU_UPLOAD_DOMAIN: '"http://up-z2.qiniu.com/"',
     QINIU_TOKEN_URL: '"http://localhost:3000/v1/qiniu-uptoken"'
 }
