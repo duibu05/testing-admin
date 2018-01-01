ab3e53fd8409dd3618443d3d72f60379d1166d72
卢纪霖 <duibu05@126.com>
2018-01-01 21:41:42 +0800

    joiner list

--- src/views/join-list/index.vue
+++ src/views/join-list/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="app-container calendar-list-container">
     <div class="filter-container">
-      <el-input @keyup.enter.native="handleFilter" style="width: 200px;" class="filter-item" placeholder="学员姓名" v-model="listQuery.stdName">
+      <el-input @keyup.enter.native="handleFilter" style="width: 200px;" class="filter-item" placeholder="学员姓名" v-model="listQuery.keyword">
       </el-input>
 
-      <el-input @keyup.enter.native="handleFilter" style="width: 200px;" class="filter-item" placeholder="手机号码" v-model="listQuery.stdPhone">
+      <el-input @keyup.enter.native="handleFilter" style="width: 200px;" class="filter-item" placeholder="手机号码" v-model="listQuery.cellphone">
       </el-input>
-
-      <el-select @change='handleFilter' style="width: 120px" class="filter-item" v-model="listQuery.type" placeholder="类别">
+      
+      <el-select clearable @change='handleFilter' style="width: 120px" class="filter-item" v-model="listQuery['joinIn.category']" placeholder="类别">
         <el-option v-for="item in ptypeOptions" :key="item.key" :label="item.label" :value="item.key">
         </el-option>
       </el-select>
@@ -21,35 +21,35 @@
         align="center"
         label="序号">
         <template scope="scope">
-            <span>{{scope.row.id}}</span>
+          <span>{{scope.$index+1+(listQuery.page-1)*listQuery.limit}}</span>
         </template>
       </el-table-column>
       <el-table-column
-        prop="stdName"
+        prop="name"
         label="学员姓名">
       </el-table-column>
       <el-table-column
-        prop="stdPhone"
+        prop="cellphone"
         label="手机号码">
       </el-table-column>
       <el-table-column
-        prop="ptype"
-        label="全部分类">
+        prop="joinIn.category"
+        label="分类">
       </el-table-column>
       <el-table-column
-        prop="pid"
+        prop="joinIn.sn"
         label="项目编号">
       </el-table-column>
       <el-table-column
-        prop="pname"
+        prop="joinIn.name"
         label="报名项目">
       </el-table-column>
       <el-table-column
-        prop="joinFrom"
+        prop="from"
         label="报名渠道">
       </el-table-column>
       <el-table-column
-        prop="meta.joinAt"
+        prop="joinAt"
         label="报名时间">
       </el-table-column>
     </el-table>
@@ -63,7 +63,7 @@
 </template>
 
 <script>
-import { fetchList } from '@/api/joiner'
+import { fetchList } from '@/api/restful'
 import waves from '@/directive/waves/index.js' // 水波纹指令
 import { parseTime } from '@/utils'
 
@@ -80,21 +80,29 @@ export default {
       listQuery: {
         page: 1,
         limit: 20,
-        stdName: undefined,
-        stdPhone: undefined,
-        type: undefined
+        keyword: undefined,
+        cellphone: undefined,
+        'joinIn.category': undefined
       },
-      ptypeOptions: [{ label: '全部', key: '' }, { label: '考试报名', key: 'test' }, { label: '课程报名', key: 'lesson' }],
+      ptypeOptions: [{ label: '全部', key: '' }, { label: '考试辅导', key: '考试辅导' }],
       tableKey: 0
     }
   },
   created() {
     this.getList()
+    fetchList('category/rebuild', { type: 'web-content' }).then(res => {
+      res.data.first.list.forEach(v => {
+        return this.ptypeOptions.push({
+          label: v.name,
+          key: v.name
+        })
+      })
+    })
   },
   methods: {
     getList() {
       this.listLoading = true
-      fetchList(this.listQuery).then(response => {
+      fetchList('joiners', this.listQuery).then(response => {
         this.list = response.data.list
         this.total = response.data.total || 0
         this.listLoading = false
