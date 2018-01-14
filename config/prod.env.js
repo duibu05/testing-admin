025e06abf23bd0d7770407bcc1f88b9f42ded537
卢纪霖 <duibu05@126.com>
2018-01-14 17:13:37 +0800

    https config

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,10 +1,10 @@
 module.exports = {
     NODE_ENV: '"production"',
     ENV_CONFIG: '"prod"',
-    BASE_API: '"http://api.gdpassing.com/v1"',
+    BASE_API: '"https://api.gdpassing.com/v1"',
     APP_ORIGIN: '"http://admin.gdpassing.com"',
     QINIU_DOWNLOAD_DOMAIN: '"http://cdn.gdpassing.com/"',
     QINIU_UPLOAD_DOMAIN: '"http://up-z2.qiniu.com/"', 
-    QINIU_TOKEN_URL: '"http://api.gdpassing.com/v1/qiniu-uptoken"'
+    QINIU_TOKEN_URL: '"https://api.gdpassing.com/v1/qiniu-uptoken"'
 };
 // TODO 生产环境请替换正式路径
